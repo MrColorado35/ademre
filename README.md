e057b3dae01c178bf1456caecf5d2531db8b00c7
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-05-30 19:06:26 +0000

    add structure and basic content

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Made by Stan Kaczorkiewicz
 
 As many of us are wearing masks in this uncertain time, the need of expression of our emotions in different way than normally is bigger than ever.
 This is the great occasion to actually create the left-hand sign language just like in the Adem world.
+
+Most of the code for this application I've took from my other app called "Pol_speak". As both of them have similar purposes- which is to teach user some sort of new language- I've decided to copy the same paths for the database.
--- app.py
+++ app.py
@@ -1,11 +1,80 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# os.environ.get('MONGO_URI')
+app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster-vdori.mongodb.net/ademre?retryWrites=true&w=majority"
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "ademre"
+COLLECTION_NAME = "words"
+
+mongo = PyMongo(app)
+
+words = mongo.db.words.find()
+words_list = [word for word in words]
+
 @app.route('/')
-def hello():
-    return "Hello boys and girls"
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/admin.html')
+def admin():
+    return render_template("admin.html")
+
+@app.route('/all_words.html')
+def all_words():
+    words = mongo.db.words.find()
+    return render_template("all_words.html", words=words)
+
+
+@app.route('/add_word.html')
+def add_word():
+    return render_template("add_word.html", words=words)
+
+
+@app.route('/insert_word', methods=['POST'])
+def insert_word():
+    words = mongo.db.words
+    words.insert_one(request.form.to_dict())
+    return redirect(url_for('all_words'))
+
+
+@app.route('/edit_word.html/<word_id>')
+def edit_word(word_id):
+    the_word = mongo.db.words.find_one({'_id': ObjectId(word_id)})    
+    return render_template("edit_word.html", word=the_word, categories=words_list)
+
+
+@app.route('/update_word/<word_id>', methods=['POST'])
+def update_word(word_id):
+    words = mongo.db.words
+    words.update({'_id': ObjectId(word_id)}, 
+    {
+        'eng': request.form.get('eng'),     
+        'gesture': request.form.get('gesture'),
+        'name': request.form.get('name'),
+        'more_info': request.form.get('more_info')
+    })
+        
+    return redirect(url_for('all_words'))
+
+
+@app.route('/delete_word.html/<word_id>')
+def delete_word(word_id):
+    the_word = mongo.db.words.find_one({'_id': ObjectId(word_id)})  
+    return render_template("delete_word.html", word=the_word)
+
+
+@app.route('/remove_word/<word_id>', methods=['POST', 'GET'])
+def remove_word(word_id):
+    mongo.db.words.remove({'_id': ObjectId(word_id)})
+    return redirect(url_for('all_words'))
+
 
 
 if __name__ == '__main__':
